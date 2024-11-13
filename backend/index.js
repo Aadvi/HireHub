14ab63c8410ac5816cf082682ec779db23533185
Aadvi <avs.rasra99@gmail.com>
2024-11-13 15:14:10 +0530

    created models for user job application and company. Added utils

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+dotenv.config({});
 
 const app = express();
-
-
 
 // middleware
 app.use(express.json());
@@ -17,7 +18,8 @@ const corsOption = {
 };
 app.use(cors(corsOption));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running at ${PORT}`);
 });
